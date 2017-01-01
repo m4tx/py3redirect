79a966637c15904828102c20675eae9326071909
Mateusz Maćkowski <m4tx@m4tx.pl>
2017-01-02 00:08:49 +0100

    Add py2->py3 pages cache (for immediate redirect)

--- background.js
+++ background.js
@@ -4,16 +4,22 @@
 
     /**
      * Check whether given URL returns 200 HTTP status code and redirects
-     * to it if so
+     * to it if so.
      *
-     * @param url Python 3 docs URL
+     * Also, save the original URL in the localStorage so the onBeforeRequest
+     * listener can redirect immediately next time visiting the same page.
+     *
+     * @param {string} oldUrl the original Python 2 docs URL that should be
+     *  cached in localStorage
+     * @param {string} url Python 3 docs URL
      * @param tabId current tab ID
      */
-    function checkDocsExist(url, tabId) {
+    function checkDocsExist(oldUrl, url, tabId) {
         let request = new XMLHttpRequest();
         request.onreadystatechange = function() {
             if (request.readyState === 4) { // DONE
                 if (request.status === 200) {
+                    localStorage.setItem(oldUrl, true);
                     chrome.tabs.update(tabId, {url: url});
                     chrome.pageAction.show(tabId);
                 } else {
@@ -29,12 +35,23 @@
         request.send();
     }
 
+    chrome.webRequest.onBeforeRequest.addListener(
+        function(details) {
+            let url = details.url;
+            if (localStorage.getItem(url)) {
+                return {redirectUrl: url.replace(URL_REGEX, URL_REPLACEMENT)};
+            }
+        },
+        {urls: ['*://docs.python.org/2*']},
+        ["blocking"]
+    );
+
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         if (request.action == "redirect") {
             let tabId = sender.tab.id;
             if (URL_REGEX.test(sender.tab.url)) {
                 chrome.pageAction.setTitle({tabId: tabId, title: 'Redirecting...'});
-                checkDocsExist(sender.tab.url.replace(URL_REGEX, URL_REPLACEMENT), tabId);
+                checkDocsExist(sender.tab.url, sender.tab.url.replace(URL_REGEX, URL_REPLACEMENT), tabId);
             }
             chrome.pageAction.show(tabId);
         }
